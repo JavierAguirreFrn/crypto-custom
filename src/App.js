e570c1410e6fe79ab4320c5c0db22da9c3916282
Javier Aguirre <javieraguirref@outlook.comgit config --listgit config --global user.email javieraguirref@outlook.com>
2022-08-20 12:13:55 -0500

    minus alert

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const [search, setSearch]= useState([])
 
 
 const CoinGeckoCallApi = async () => {
-  alert("entra")
+  // alert("entra")
 
   const response = await axios.get(
     "https://api.coingecko.com/api/v3/coins/markets?vs_currency=mxn&order=market_cap_desc&per_page=100&page=1&sparkline=false"
